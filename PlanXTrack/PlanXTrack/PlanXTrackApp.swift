afdc423563ca87d2e1a983a11852098f40a0c757
Adam Londa <adamlonda@pm.me>
2024-11-09 12:30:58 +0100

    Blank project

---
+++ PlanXTrack/PlanXTrack/PlanXTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanXTrackApp.swift
+//  PlanXTrack
+//
+//  Created by Adam Londa on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanXTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
