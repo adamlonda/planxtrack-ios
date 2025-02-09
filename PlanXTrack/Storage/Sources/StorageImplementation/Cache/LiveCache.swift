a573b521eef269bc5bd9006e5acf50b118ec9519
Adam Londa <adamlonda@pm.me>
2025-02-09 14:01:07 +0100

    WIP: Cache

---
+++ PlanXTrack/Storage/Sources/StorageImplementation/Cache/LiveCache.swift
@@ -0,0 +1,19 @@
+//
+//  LiveCache.swift
+//  Storage
+//
+//  Created by Adam Londa on 15.02.2025.
+//
+
+import Model
+import Storage
+
+public final class LiveCache: Cache {
+    public func save(_ record: PlankRecord) {
+        // TODO: Implement 🚧
+    }
+
+    public func load() -> [PlankRecord] {
+        [] // TODO: Implement 🚧
+    }
+}
